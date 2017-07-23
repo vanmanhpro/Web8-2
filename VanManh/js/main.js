4eeed5d525dfa060afe749421f05b63acb087c74
vanmanhpro <vanmanhpbc1998@gmail.com>
2017-07-23 21:55:26 +0700

    Player added

--- VanManh/js/main.js
+++ VanManh/js/main.js
@@ -1,8 +1,14 @@
 var Nakama = {};
-Nakama.configs = {};
+Nakama.configs = {
+  GAME_WIDTH: 640,
+  GAME_HEIGHT: 960
+};
 
 window.onload = function(){
-  Nakama.game = new Phaser.Game(640,960,Phaser.AUTO,'',
+  Nakama.game = new Phaser.Game(
+    Nakama.configs.GAME_WIDTH,
+    Nakama.configs.GAME_HEIGHT,
+    Phaser.AUTO,'',
     {
       preload: preload,
       create: create,
@@ -14,10 +20,10 @@ window.onload = function(){
 
 // preparations before game starts
 var preload = function(){
-  Nakama.game.scale.minWidth = 320;
-  Nakama.game.scale.minHeight = 480;
-  Nakama.game.scale.maxWidth = 640;
-  Nakama.game.scale.maxHeight = 960;
+  Nakama.game.scale.minWidth = Nakama.configs.GAME_WIDTH/2;
+  Nakama.game.scale.minHeight =  Nakama.configs.GAME_HEIGHT/2;
+  Nakama.game.scale.maxWidth =  Nakama.configs.GAME_WIDTH;
+  Nakama.game.scale.maxHeight =  Nakama.configs.GAME_HEIGHT;
   Nakama.game.scale.pageAlignHorizontally = true;
   Nakama.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
@@ -31,11 +37,30 @@ var preload = function(){
 var create = function(){
   Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
   Nakama.keyboard = Nakama.game.input.keyboard;
+
+  Nakama.background = Nakama.game.add.sprite(0, -960, 'background');
+  Nakama.player = Nakama.game.add.sprite(200, 400, 'assets', 'Spaceship2Right-Player.png');
 }
 
 // update game state each frame
 var update = function(){
+  if(Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)){
+    Nakama.player.position.x = Math.max( 0, Nakama.player.position.x - 10);
+  }
+  if(Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+    //console(Nakama.player.position.y);
+    Nakama.player.position.x = Math.min( Nakama.configs.GAME_WIDTH - Nakama.player.width, Nakama.player.position.x + 10);
+  }
+  if(Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)){
+    //console(Nakama.player.position.y);
+    Nakama.player.position.y = Math.min( Nakama.configs.GAME_HEIGHT - Nakama.player.height, Nakama.player.position.y + 10);
+  }
+  if(Nakama.keyboard.isDown(Phaser.Keyboard.UP)){
+    Nakama.player.position.y = Math.max( 0, Nakama.player.position.y - 10);
+  }
 
+  Nakama.background.position.y += 10;
+  if (Nakama.background.position.y > 0) Nakama.background.position.y -= 960;
 }
 
 // before camera render (mostly for debug)
